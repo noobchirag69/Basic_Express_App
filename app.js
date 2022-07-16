1d2645a3b62cca515910058a93353d6ae2992003
noobchirag69 <chiragchakraborty48@gmail.com>
2022-07-16 09:53:00 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// Importing Express JS
+const express = require('express');
+
+// Creating Express App
+const app = express();
+
+// Setting the directory for serving static files
+app.use(express.static('views'));
+
+// Listening for requests on Port 3000
+app.listen(3000);
+
+// Sending the Index page
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', { root: __dirname });
+});
+
+// Sending the About page
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname });
+});
+
+// Redirecting
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+// Sending the Error Page
+app.use((req, res) => {
+    res.status(404).sendFile('./views/error.html', { root: __dirname });
+});
